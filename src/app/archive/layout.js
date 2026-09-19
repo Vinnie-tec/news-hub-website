e3f472eb45e7957c20012e02395f0dea6c017e3c
Agboola Vincent <agboolavin65@gmail.com>
2026-09-19 01:00:47 +0100

    Introduction to parrallel route

---
+++ src/app/archive/layout.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function ArchiveLayout({archive, latest}) {
+  return (
+    <div>
+        <h1>News Archive</h1>
+        <section id="archive-filter">
+            {archive}
+        </section>
+        <section id="latest-filter">
+            {latest}
+        </section>
+    </div>
+  )
+}
